1b72df962ae025f5c1a54450b25f5ff988020df9
lxcalbxy <ycxsbeats@yandex.ru>
2026-04-09 00:50:23 +0300

    fix: improve Money VO

--- src/main/java/lv/v3nom/domain/value/Money.java
+++ src/main/java/lv/v3nom/domain/value/Money.java
@@ -1,49 +1,94 @@
 package lv.v3nom.domain.value;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
 
-public class Money {
+public final class Money implements Comparable<Money>{
     private final BigDecimal amount;
 
-    public Money(BigDecimal amount) {
-        if (amount == null) {
-            throw new IllegalArgumentException("Money amount cannot be null");
-        }
+    public static final Money ZERO =
+            new Money(BigDecimal.ZERO);
+
+    private Money(BigDecimal amount) {
+        Objects.requireNonNull(amount, "Money amount cannot be null");
         if (amount.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("Error passing negative value");
+            throw new IllegalArgumentException("Money cannot be negative" + amount);
         } else {
-            this.amount = amount;
+            this.amount = amount.setScale(2, RoundingMode.HALF_EVEN);
         }
     }
+
+    public static Money of(BigDecimal amount) {
+        return new Money(amount);
+    }
+    public static Money of(String amount) {
+        return new Money(new BigDecimal(amount));
+    }
+    public static Money of(long amount) {
+        return new Money(BigDecimal.valueOf(amount));
+    }
+
+    /*
+    wrong shit
 
     public Money add(BigDecimal amountToAdd) {
         BigDecimal newAmount = this.amount.add(amountToAdd);
         return new Money(newAmount);
+    }*/
+
+    // MATH
+    public Money add(Money other) {
+        return new Money(this.amount.add(other.amount));
+    }
+    public Money subtract(Money other) {
+        return new Money(this.amount.subtract(other.amount));
+    }
+    public Money multiply(int multiplier) {
+        return new Money(this.amount.multiply(BigDecimal.valueOf(multiplier)));
     }
 
-    public Money subtract(BigDecimal amountToSubstract) {
-        BigDecimal newAmount = this.amount.subtract(amountToSubstract);
-        return new Money(newAmount);
+    // COMPARSION
+    public boolean isGreaterThan(Money other) {
+        return this.amount.compareTo(other.amount) > 0;
     }
-
+    public boolean isLessThan(Money other) {
+        return this.amount.compareTo(other.amount) < 0;
+    }
+    public boolean isGreaterThanOrEqualTo(Money other) {
+        return this.amount.compareTo(other.amount) >= 0;
+    }
+    public boolean isNegativeOrZero() {
+        return this.amount.compareTo(BigDecimal.ZERO) <= 0;
+    }
+    public boolean isZero() {
+        return this.amount.compareTo(BigDecimal.ZERO) == 0;
+    }
     public boolean isPositive() {
-        return amount.compareTo(BigDecimal.ZERO) > 0;
+        return this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+    public BigDecimal getAmount() {
+        return this.amount;
     }
 
-    public boolean isZeroOrPositive() {
-        return amount.compareTo(BigDecimal.ZERO) >= 0;
+    // HELPERS
+    @Override
+    public int compareTo(Money other) {
+        return this.amount.compareTo(other.amount);
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Money money = (Money) o;
-        return amount.compareTo(money.amount) == 0;
+        return this.amount.compareTo(money.amount) == 0;
     }
-
     @Override
     public int hashCode() {
-        return amount.hashCode();
+        return this.amount.hashCode();
+    }
+    @Override
+    public String toString() {
+        return this.amount.toPlainString();
     }
 }
