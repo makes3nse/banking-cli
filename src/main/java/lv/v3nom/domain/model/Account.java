46e7d3f439dda634bb6334e7b6a00a06066c6e88
lxcalbxy <ycxsbeats@yandex.ru>
2026-04-09 00:51:56 +0300

    feat: add deposit + small fix

--- src/main/java/lv/v3nom/domain/model/Account.java
+++ src/main/java/lv/v3nom/domain/model/Account.java
@@ -1,12 +1,16 @@
 package lv.v3nom.domain.model;
 
 import lv.v3nom.domain.value.*;
+import lv.v3nom.infrastructure.util.impl.SystemDateTimeProvider;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
-    private AccountId accountId = AccountId.generate();
+    private SystemDateTimeProvider clock = new SystemDateTimeProvider();
+
+    private AccountId accountId;
     private CustomerId ownerId;
     private Money balance;
     private AccountStatus accountStatus;
@@ -14,37 +18,29 @@ public class Account {
 
     private List<TransactionId> transactionHistory;
 
-    public AccountId getAccountId() {
-        return accountId;
-    }
-    public CustomerId getOwnerId() {
-        return ownerId;
-    }
-    public void setOwnerId(CustomerId ownerId) {
+    public Account(CustomerId ownerId) {
+        this.accountId = AccountId.generate();
         this.ownerId = ownerId;
+        this.balance = Money.ZERO;
+        this.accountStatus = AccountStatus.ACTIVE;
+        this.createdAt = clock.now();
+        this.transactionHistory = new ArrayList<>();
     }
-    public Money getBalance() {
-        return balance;
+
+    public void deposit(Money amount) {
+        if (amount.isNegativeOrZero()) {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+        if (accountStatus != AccountStatus.ACTIVE) {
+            throw new IllegalStateException("Deposits are disabled for your account");
+        }
+        this.balance = this.balance.add(amount);
     }
-    public void setBalance(Money balance) {
-        this.balance = balance;
-    }
-    public AccountStatus getAccountStatus() {
-        return accountStatus;
-    }
-    public void setAccountStatus(AccountStatus accountStatus) {
-        this.accountStatus = accountStatus;
-    }
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-    public List<TransactionId> getTransactionHistory() {
-        return transactionHistory;
-    }
-    public void setTransactionHistory(List<TransactionId> transactionHistory) {
-        this.transactionHistory = transactionHistory;
-    }
+
+    public AccountId getAccountId() { return accountId; }
+    public CustomerId getOwnerId() { return ownerId; }
+    public Money getBalance() { return balance; }
+    public AccountStatus getAccountStatus() { return accountStatus; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public List<TransactionId> getTransactionHistory() { return transactionHistory; }
 }
