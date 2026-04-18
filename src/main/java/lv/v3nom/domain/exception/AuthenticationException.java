d6e58b891a8fa9b45b649621ad178749ca28f7f2
lxcalbxy <ycxsbeats@yandex.ru>
2026-04-19 01:07:26 +0300

    feat: add AuthenticationException

--- src/main/java/lv/v3nom/domain/exception/AuthenticationException.java
+++ src/main/java/lv/v3nom/domain/exception/AuthenticationException.java
@@ -1,4 +1,15 @@
 package lv.v3nom.domain.exception;
 
-public class AuthenticationException {
+import lv.v3nom.domain.value.CustomerId;
+
+public class AuthenticationException extends RuntimeException {
+    private final CustomerId customerId;
+
+    public AuthenticationException(CustomerId customerId) {
+        super(String.format("Authentication error for user %s", customerId.toString()));
+
+        this.customerId = customerId;
+    }
+
+    public CustomerId getCustomerId() { return this.customerId; }
 }
