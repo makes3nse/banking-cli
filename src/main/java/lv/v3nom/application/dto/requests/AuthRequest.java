461cd1894eacdce75bc371f0a8b791e0089284ad
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-20 04:12:23 +0300

    refactor: mend login logic. No sessionToken is needed

--- src/main/java/lv/v3nom/application/dto/requests/AuthRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/AuthRequest.java
@@ -1,24 +1,20 @@
 package lv.v3nom.application.dto.requests;
 
 public class AuthRequest {
-    private String currentSessionToken;
     private String idempotencyKey;
     private String email;
     private String password;
 
     public AuthRequest() {} // for possible frameworks
-    public AuthRequest(String currentSessionToken,
-                       String idempotencyKey,
+    public AuthRequest(String idempotencyKey,
                        String email,
                        String password) {
 
-        this.currentSessionToken = currentSessionToken;
         this.idempotencyKey = idempotencyKey;
         this.email = email;
         this.password = password;
     }
 
-    public String getCurrentSessionToken() { return currentSessionToken; }
     public String getIdempotencyKey() { return idempotencyKey; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
