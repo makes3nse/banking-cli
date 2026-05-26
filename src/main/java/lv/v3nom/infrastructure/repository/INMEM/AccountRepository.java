67a2c6f5aa952302530233f5582a2bfa00c83240
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-26 22:32:53 +0300

    feat: add AccountRepository interface

--- src/main/java/lv/v3nom/infrastructure/repository/INMEM/AccountRepository.java
+++ src/main/java/lv/v3nom/infrastructure/repository/INMEM/AccountRepository.java
@@ -1,4 +1,14 @@
 package lv.v3nom.infrastructure.repository.INMEM;
 
+import lv.v3nom.domain.model.Account;
+import lv.v3nom.domain.value.AccountId;
+import lv.v3nom.domain.value.CustomerId;
+
+import java.util.List;
+
 public interface AccountRepository {
+    public void save(Account account);
+    public Account findById(AccountId accountId);
+    public List<Account> findAll();
+    public List<Account> findByCustomerId(CustomerId customerId);
 }
