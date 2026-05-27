9d9f64544fdf724a717c59ddb16e6cd8ad316f5f
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-27 21:36:41 +0300

    feat: add CustomerRepository interface

--- src/main/java/lv/v3nom/infrastructure/repository/INMEM/CustomerRepository.java
+++ src/main/java/lv/v3nom/infrastructure/repository/INMEM/CustomerRepository.java
@@ -1,4 +1,15 @@
 package lv.v3nom.infrastructure.repository.INMEM;
 
+import lv.v3nom.domain.model.Customer;
+import lv.v3nom.domain.value.CustomerId;
+import lv.v3nom.domain.value.EmailAddress;
+
+import java.util.List;
+
 public interface CustomerRepository {
+    public void save(Customer customer);
+    public Customer findById(CustomerId customerId);
+    public List<Customer> findAll();
+    public Customer findByEmail(EmailAddress emailAddress);
+    public boolean existsByEmail(EmailAddress emailAddress);
 }
