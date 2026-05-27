9baa5fbaac5fb2178a986fb76e53f03ff683e4dd
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-27 21:37:23 +0300

    feat: add TransactionRepository interface

--- src/main/java/lv/v3nom/infrastructure/repository/INMEM/TransactionRepository.java
+++ src/main/java/lv/v3nom/infrastructure/repository/INMEM/TransactionRepository.java
@@ -1,4 +1,18 @@
 package lv.v3nom.infrastructure.repository.INMEM;
 
+import lv.v3nom.domain.model.Transaction;
+import lv.v3nom.domain.value.AccountId;
+import lv.v3nom.domain.value.TransactionId;
+import lv.v3nom.domain.value.TransactionStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TransactionRepository {
+    public void save(Transaction transaction);
+    public Transaction findById(TransactionId transactionId);
+    public List<Transaction> findAll();
+    public List<Transaction> findByAccountId(AccountId accountId);
+    public List<Transaction> findByStatus(TransactionStatus transactionStatus);
+    public List<Transaction> findByDateRange(LocalDateTime from, LocalDateTime to);
 }
