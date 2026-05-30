3d14352e5c47764b4410ee05bd76cb2ea57d3f4d
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-31 02:48:36 +0300

    feat: add currency to DepositRequest

--- src/main/java/lv/v3nom/application/dto/requests/DepositRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/DepositRequest.java
@@ -6,22 +6,26 @@ public class DepositRequest {
     private String currentSessionToken;
     private String idempotencyKey;
     private String accountId;
+    private String currency;
     private BigDecimal amount;
 
     public DepositRequest() {}
     public DepositRequest(String currentSessionToken,
                           String idempotencyKey,
                           String accountId,
+                          String currency,
                           BigDecimal amount) {
 
         this.currentSessionToken = currentSessionToken;
         this.idempotencyKey = idempotencyKey;
         this.accountId = accountId;
+        this.currency = currency;
         this.amount = amount;
     }
 
     public String getCurrentSessionToken() { return currentSessionToken; }
     public String getIdempotencyKey() { return idempotencyKey; }
     public String getAccountId() { return accountId; }
+    public String getCurrency() { return currency; }
     public BigDecimal getAmount() { return amount; }
 }
