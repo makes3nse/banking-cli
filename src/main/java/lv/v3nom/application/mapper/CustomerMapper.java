5bb40f7cbcaf1f7e46881eae319700ed9e5c10f7
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-31 02:52:31 +0300

    feat: add CustomerMapper

---
+++ src/main/java/lv/v3nom/application/mapper/CustomerMapper.java
@@ -0,0 +1,33 @@
+package lv.v3nom.application.mapper;
+
+import lv.v3nom.application.dto.requests.RegisterCustomerRequest;
+import lv.v3nom.application.dto.responses.CustomerResponse;
+import lv.v3nom.domain.model.Customer;
+import lv.v3nom.domain.security.PasswordHasher;
+import lv.v3nom.domain.value.OperationStatus;
+
+import java.time.LocalDateTime;
+
+public class CustomerMapper {
+    public static Customer toDomain(RegisterCustomerRequest request, PasswordHasher hasher, LocalDateTime now) {
+        return Customer.register(
+                request.getName(),
+                request.getEmail(),
+                request.getPhone(),
+                request.getRawPassword(),
+                hasher,
+                now
+        );
+    }
+    public static CustomerResponse toResponse(Customer customer, OperationStatus operationStatus) {
+        return new CustomerResponse(
+                customer.getId().getValue(),
+                customer.getName(),
+                customer.getEmail().getValue(),
+                customer.getPhoneNumber().getValue(),
+                customer.getCustomerStatus().getValue(),
+                operationStatus.getValue(),
+                operationStatus.getDescription()
+        );
+    }
+}
