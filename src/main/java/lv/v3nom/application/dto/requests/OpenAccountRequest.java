a10b39adca878849db96945293fd0bc0dfd742a5
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-31 02:48:52 +0300

    feat: add currency to OpenAccountRequest

--- src/main/java/lv/v3nom/application/dto/requests/OpenAccountRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/OpenAccountRequest.java
@@ -4,18 +4,22 @@ public class OpenAccountRequest {
     private String currentSessionToken;
     private String idempotencyKey;
     private String customerId;
+    private String currency;
 
     public OpenAccountRequest() {}
     public OpenAccountRequest(String currentSessionToken,
                               String idempotencyKey,
-                              String customerId) {
+                              String customerId,
+                              String currency) {
 
         this.currentSessionToken = currentSessionToken;
         this.idempotencyKey = idempotencyKey;
         this.customerId = customerId;
+        this.currency = currency;
     }
 
-    public String currentSessionToken() { return currentSessionToken;}
+    public String getCurrentSessionToken() { return currentSessionToken;}
     public String getIdempotencyKey() { return idempotencyKey; }
     public String getCustomerId() { return customerId; }
+    public String getCurrency() { return currency; }
 }
