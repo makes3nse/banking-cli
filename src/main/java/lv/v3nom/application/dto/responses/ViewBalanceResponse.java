ea799225d777d51676cc080ed7f5dbc778fdad0f
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-30 17:11:36 +0300

    fix: add missing Operation Status and probable Error Message placeholders

--- src/main/java/lv/v3nom/application/dto/responses/ViewBalanceResponse.java
+++ src/main/java/lv/v3nom/application/dto/responses/ViewBalanceResponse.java
@@ -6,17 +6,26 @@ public class ViewBalanceResponse {
     private String accountId;
     private String status;
     private BigDecimal balance;
+    private String operationStatus;
+    private String errorMessage;
 
     public ViewBalanceResponse() {}
     public ViewBalanceResponse(String accountId,
                                String status,
-                               BigDecimal balance) {
+                               BigDecimal balance,
+                               String operationStatus,
+                               String errorMessage) {
+
         this.accountId = accountId;
         this.status = status;
         this.balance = balance;
+        this.operationStatus = operationStatus;
+        this.errorMessage = errorMessage;
     }
 
     public String getAccountId() { return accountId; }
     public String getStatus() { return status; }
     public BigDecimal getBalance() { return balance; }
+    public String getOperationStatus() { return operationStatus; }
+    public String getErrorMessage() { return errorMessage; }
 }
