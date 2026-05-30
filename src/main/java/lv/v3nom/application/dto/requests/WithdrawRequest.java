0c2cb2a36b3d4a52f60e4150ca88b056ba6885a2
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-31 02:49:33 +0300

    feat: add currency to WithdrawRequest

--- src/main/java/lv/v3nom/application/dto/requests/WithdrawRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/WithdrawRequest.java
@@ -6,22 +6,26 @@ public class WithdrawRequest {
     private String currentSessionToken;
     private String idempotencyKey;
     private String accountId;
+    private String currency;
     private BigDecimal amount;
 
     public WithdrawRequest() {}
     public WithdrawRequest(String currentSessionToken,
                            String idempotencyKey,
                            String accountId,
+                           String currency,
                            BigDecimal amount) {
 
         this.currentSessionToken = currentSessionToken;
         this.idempotencyKey = idempotencyKey;
         this.accountId = accountId;
+        this.currency = currency;
         this.amount = amount;
     }
 
     public String getCurrentSessionToken() { return currentSessionToken; }
     public String getIdempotencyKey() { return idempotencyKey; }
     public String getAccountId() { return accountId; }
+    public String getCurrency() { return currency; }
     public BigDecimal getAmount() { return amount; }
 }
