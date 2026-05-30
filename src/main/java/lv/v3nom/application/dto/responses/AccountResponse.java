85342af2917ce8589543c57a27999b0cc5d79616
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-30 17:10:45 +0300

    fix: add missing Operation Status and probable Error Message placeholders

--- src/main/java/lv/v3nom/application/dto/responses/AccountResponse.java
+++ src/main/java/lv/v3nom/application/dto/responses/AccountResponse.java
@@ -7,21 +7,29 @@ public class AccountResponse {
     private String customerId;
     private String status;
     private BigDecimal balance;
+    private String operationStatus;
+    private String errorMessage;
 
     public AccountResponse() {}
     public AccountResponse(String accountId,
                            String customerId,
                            String status,
-                           BigDecimal balance) {
+                           BigDecimal balance,
+                           String operationStatus,
+                           String errorMessage) {
 
         this.accountId = accountId;
         this.customerId = customerId;
         this.status = status;
         this.balance = balance;
+        this.operationStatus = operationStatus;
+        this.errorMessage = errorMessage;
     }
 
     public String getAccountId() { return accountId; }
     public String getCustomerId() { return customerId; }
     public String getStatus() { return status; }
     public BigDecimal getBalance() { return balance; }
+    public String getOperationStatus() { return operationStatus; }
+    public String getErrorMessage() { return errorMessage; }
 }
