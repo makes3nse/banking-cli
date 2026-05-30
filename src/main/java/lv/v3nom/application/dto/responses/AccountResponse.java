0d060a5554276c1c743719cb4071575d6fe27ec4
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-31 02:50:07 +0300

    feat: add currency AccountResponse

--- src/main/java/lv/v3nom/application/dto/responses/AccountResponse.java
+++ src/main/java/lv/v3nom/application/dto/responses/AccountResponse.java
@@ -6,6 +6,7 @@ public class AccountResponse {
     private String accountId;
     private String customerId;
     private String status;
+    private String currency;
     private BigDecimal balance;
     private String operationStatus;
     private String errorMessage;
@@ -14,6 +15,7 @@ public class AccountResponse {
     public AccountResponse(String accountId,
                            String customerId,
                            String status,
+                           String currency,
                            BigDecimal balance,
                            String operationStatus,
                            String errorMessage) {
@@ -21,6 +23,7 @@ public class AccountResponse {
         this.accountId = accountId;
         this.customerId = customerId;
         this.status = status;
+        this.currency = currency;
         this.balance = balance;
         this.operationStatus = operationStatus;
         this.errorMessage = errorMessage;
@@ -29,6 +32,7 @@ public class AccountResponse {
     public String getAccountId() { return accountId; }
     public String getCustomerId() { return customerId; }
     public String getStatus() { return status; }
+    public String getCurrency() { return currency; }
     public BigDecimal getBalance() { return balance; }
     public String getOperationStatus() { return operationStatus; }
     public String getErrorMessage() { return errorMessage; }
