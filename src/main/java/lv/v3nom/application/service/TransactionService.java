de3b210bf5720c227153e199fae562ecf80c58a2
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-02 00:24:11 +0300

    feat: add TransactionService interface

--- src/main/java/lv/v3nom/application/service/TransactionService.java
+++ src/main/java/lv/v3nom/application/service/TransactionService.java
@@ -1,4 +1,11 @@
 package lv.v3nom.application.service;
 
-public class TransactionService {
+import lv.v3nom.application.dto.requests.TransactionDetailsRequest;
+import lv.v3nom.application.dto.requests.TransactionHistoryRequest;
+import lv.v3nom.application.dto.responses.TransactionHistoryResponse;
+import lv.v3nom.application.dto.responses.TransactionResponse;
+
+public interface TransactionService {
+    public TransactionHistoryResponse getTransactionHistory(TransactionHistoryRequest request);
+    public TransactionResponse getTransactionDetails(TransactionDetailsRequest request);
 }
