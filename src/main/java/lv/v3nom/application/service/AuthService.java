ef5d27a194ee76ef75e86b5bea15260f2bfbb24d
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-02 00:20:12 +0300

    feat: add AuthService interface

--- src/main/java/lv/v3nom/application/service/AuthService.java
+++ src/main/java/lv/v3nom/application/service/AuthService.java
@@ -1,4 +1,12 @@
 package lv.v3nom.application.service;
 
-public class AuthService {
+import lv.v3nom.application.dto.requests.AuthRequest;
+import lv.v3nom.application.dto.requests.LogOutRequest;
+import lv.v3nom.application.dto.responses.AuthResponse;
+import lv.v3nom.domain.value.CustomerId;
+
+public interface AuthService {
+    public AuthResponse login(AuthRequest request);
+    public void logout(LogOutRequest request);
+    public CustomerId validateToken(String tokenValue); // Internal use only, not exposed to CLI
 }
