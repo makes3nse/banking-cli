bca5fcc32901dd77177af82f5dfab3ba0e17136c
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-02 00:17:19 +0300

    fix: remove 'customerId' from this request as it's can be retrieved from token itself

--- src/main/java/lv/v3nom/application/dto/requests/LogOutRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/LogOutRequest.java
@@ -3,19 +3,15 @@ package lv.v3nom.application.dto.requests;
 public class LogOutRequest {
     private String currentSessionToken;
     private String idempotencyKey;
-    private String customerId;
 
     public LogOutRequest() {}
     public LogOutRequest(String currentSessionToken,
-                         String idempotencyKey,
-                         String customerId) {
+                         String idempotencyKey) {
 
         this.currentSessionToken = currentSessionToken;
         this.idempotencyKey = idempotencyKey;
-        this.customerId = customerId;
     }
 
     public String getCurrentSessionToken() { return currentSessionToken; }
     public String getIdempotencyKey() { return idempotencyKey; }
-    public String getCustomerId() { return customerId; }
 }
