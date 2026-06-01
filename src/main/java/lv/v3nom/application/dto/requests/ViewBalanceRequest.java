bb2c5d65e31f8a0b6e377917dadd0e4571c23e1c
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-02 00:37:32 +0300

    fix: remove 'idempotencyKey' from this request as it's read-only

--- src/main/java/lv/v3nom/application/dto/requests/ViewBalanceRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/ViewBalanceRequest.java
@@ -2,20 +2,16 @@ package lv.v3nom.application.dto.requests;
 
 public class ViewBalanceRequest {
     private String currentSessionToken;
-    private String idempotencyKey;
     private String accountId;
 
     public ViewBalanceRequest() {}
     public ViewBalanceRequest(String currentSessionToken,
-                              String idempotencyKey,
                               String accountId) {
 
         this.currentSessionToken = currentSessionToken;
-        this.idempotencyKey = idempotencyKey;
         this.accountId = accountId;
     }
 
     public String getCurrentSessionToken() { return currentSessionToken; }
-    public String getIdempotencyKey() { return idempotencyKey; }
     public String getAccountId() { return accountId; }
 }
