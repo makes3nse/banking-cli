f1cbd4abd0109932d431c660d0f4e7d6c12af7e5
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-02 00:37:41 +0300

    fix: remove 'idempotencyKey' from this request as it's read-only

--- src/main/java/lv/v3nom/application/dto/requests/GetAccountsRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/GetAccountsRequest.java
@@ -2,20 +2,16 @@ package lv.v3nom.application.dto.requests;
 
 public class GetAccountsRequest {
     private String currentSessionToken;
-    private String idempotencyKey;
     private String customerId;
 
     public GetAccountsRequest() {}
     public GetAccountsRequest(String currentSessionToken,
-                              String idempotencyKey,
                               String customerId) {
 
         this.currentSessionToken = currentSessionToken;
-        this.idempotencyKey = idempotencyKey;
         this.customerId = customerId;
     }
 
     public String getCurrentSessionToken() { return currentSessionToken;}
-    public String getIdempotencyKey() { return idempotencyKey; }
     public String getCustomerId() { return customerId; }
 }
