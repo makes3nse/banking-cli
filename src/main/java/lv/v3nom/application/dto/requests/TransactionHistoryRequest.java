bca94d8b84b8251c6096681722d338b229758d19
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-02 00:37:23 +0300

    fix: remove 'idempotencyKey' from this request as it's read-only

--- src/main/java/lv/v3nom/application/dto/requests/TransactionHistoryRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/TransactionHistoryRequest.java
@@ -2,27 +2,23 @@ package lv.v3nom.application.dto.requests;
 
 public class TransactionHistoryRequest {
     private String currentSessionToken;
-    private String idempotencyKey;
     private String accountId;
     private String fromDate;
     private String toDate;
 
     public TransactionHistoryRequest() {}
     public TransactionHistoryRequest(String currentSessionToken,
-                                     String idempotencyKey,
                                      String accountId,
                                      String fromDate,
                                      String toDate) {
 
         this.currentSessionToken = currentSessionToken;
-        this.idempotencyKey = idempotencyKey;
         this.accountId = accountId;
         this.fromDate = fromDate;
         this.toDate = toDate;
     }
 
     public String getCurrentSessionToken() { return currentSessionToken; }
-    public String getIdempotencyKey() { return idempotencyKey; }
     public String getAccountId() { return accountId; }
     public String getFromDate() { return fromDate; }
     public String getToDate() { return toDate; }
