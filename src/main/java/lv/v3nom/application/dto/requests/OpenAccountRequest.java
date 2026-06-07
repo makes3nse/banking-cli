f2710cbc63b4facd59a8ae40d9fd191551abd750
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-08 00:06:17 +0300

    refactor: remove explicit customerId provision in request body as it's better to be retrieved from session token in the service itself

--- src/main/java/lv/v3nom/application/dto/requests/OpenAccountRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/OpenAccountRequest.java
@@ -3,23 +3,19 @@ package lv.v3nom.application.dto.requests;
 public class OpenAccountRequest {
     private String currentSessionToken;
     private String idempotencyKey;
-    private String customerId;
     private String currency;
 
     public OpenAccountRequest() {}
     public OpenAccountRequest(String currentSessionToken,
                               String idempotencyKey,
-                              String customerId,
                               String currency) {
 
         this.currentSessionToken = currentSessionToken;
         this.idempotencyKey = idempotencyKey;
-        this.customerId = customerId;
         this.currency = currency;
     }
 
     public String getCurrentSessionToken() { return currentSessionToken;}
     public String getIdempotencyKey() { return idempotencyKey; }
-    public String getCustomerId() { return  customerId; }
     public String getCurrency() { return currency; }
 }
