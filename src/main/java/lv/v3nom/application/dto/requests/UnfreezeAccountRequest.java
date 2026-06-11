7e46c94d26b03195fda96e2da55b3bdc7e8f0681
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-11 04:01:33 +0300

    feat: add UnfreezeAccountRequest DTO

---
+++ src/main/java/lv/v3nom/application/dto/requests/UnfreezeAccountRequest.java
@@ -0,0 +1,21 @@
+package lv.v3nom.application.dto.requests;
+
+public class UnfreezeAccountRequest {
+    private String currentSessionToken;
+    private String idempotencyKey;
+    private String accountId;
+
+    public UnfreezeAccountRequest() {}
+    public UnfreezeAccountRequest(String currentSessionToken,
+                                  String idempotencyKey,
+                                  String accountId) {
+
+        this.currentSessionToken = currentSessionToken;
+        this.idempotencyKey = idempotencyKey;
+        this.accountId = accountId;
+    }
+
+    public String getCurrentSessionToken() { return currentSessionToken; }
+    public String getIdempotencyKey() { return idempotencyKey; }
+    public String getAccountId() { return accountId; }
+}
