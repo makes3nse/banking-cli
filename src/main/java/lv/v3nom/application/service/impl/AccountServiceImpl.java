d12dd709aacbcf8c7d134d330501c8a1e7ab9fbb
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-12 13:02:46 +0300

    feat: add closeAccount() method implementation in Account Service

--- src/main/java/lv/v3nom/application/service/impl/AccountServiceImpl.java
+++ src/main/java/lv/v3nom/application/service/impl/AccountServiceImpl.java
@@ -384,7 +384,49 @@ public class AccountServiceImpl implements AccountService {
     }
     @Override
     public AccountResponse closeAccount(CloseAccountRequest request) {
-        return null;
+        SystemDateTimeProvider time = new SystemDateTimeProvider();
+        CustomerId authenticatedId = tokenStore.getCustomerId(request.getCurrentSessionToken());
+        AccountId accountId = AccountId.of(request.getAccountId());
+        IdempotencyKey idempotencyKey = IdempotencyKey.of(request.getIdempotencyKey());
+
+        boolean isValidToken = tokenStore.isValid(request.getCurrentSessionToken(), time.now());
+        if (authenticatedId == null || !isValidToken) {
+            String failureReason = String.format(
+                    "Token: %s; Validity: %s; User: %s;",
+                    request.getCurrentSessionToken(),
+                    isValidToken,
+                    authenticatedId
+            );
+
+            return AccountMapper.failureResponse(authenticatedId.getValue(), OperationStatus.FAILURE);
+        }
+
+        Object cachedResponse = idempotencyStore.retrieve(authenticatedId, idempotencyKey);
+        if (cachedResponse != null) {
+            return (AccountResponse) cachedResponse;
+        }
+
+        Account account = accountRepository.findById(accountId);
+        boolean isExistingAccount = account != null;
+        boolean isOwnedByAuthenticatedCustomer = account.getOwnerId().equals(authenticatedId);
+        if (!isExistingAccount || !isOwnedByAuthenticatedCustomer) {
+            String failureReason = String.format(
+                    "Account: %s; Owned: %; Exists: %s; User: %s;",
+                    request.getAccountId(),
+                    isOwnedByAuthenticatedCustomer,
+                    isExistingAccount,
+                    authenticatedId
+            );
+
+            return AccountMapper.failureResponse(failureReason, OperationStatus.FAILURE);
+        }
+
+        account.close(time.now());
+        accountRepository.save(account);
+        AccountResponse response = AccountMapper.toResponse(account, OperationStatus.SUCCESS);
+        idempotencyStore.store(authenticatedId, idempotencyKey, response);
+
+        return response;
     }
     @Override
     public AccountResponse freezeAccount(FreezeAccountRequest request) {
