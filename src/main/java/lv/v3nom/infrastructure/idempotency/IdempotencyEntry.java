80a6cc6a91c89bb968f4053f6043a07787053796
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-13 14:00:13 +0300

    refactor: remove IdempotencyEntry cause of switching to JSON object storage

--- src/main/java/lv/v3nom/infrastructure/idempotency/IdempotencyEntry.java
+++ src/main/java/lv/v3nom/infrastructure/idempotency/IdempotencyEntry.java
@@ -1,34 +1,39 @@
 package lv.v3nom.infrastructure.idempotency;
 
-public final class IdempotencyEntry { // helper VO (response + expiry bundle)
-    private Object response;
-    private long expiryTime;
+// helper VO (response + expiry bundle)
+public final class IdempotencyEntry {
+    // Redundant, Object store cannot be used outside of Monolith microservices project,
+    // So, to make things easier we are going to start using right approach straight away
+    // It's going to be much easier to do it right now than refactor it later.
 
-    private IdempotencyEntry(Object response, long expiryTime) {
-        if (response == null) {
-            throw new IllegalArgumentException("Idempotency Entry cannot be created with null values");
-        }
-
-        this.response = response;
-        this.expiryTime = expiryTime;
-    }
-
-    public static IdempotencyEntry of(Object respose, long expiryTime) {
-        return new IdempotencyEntry(respose, expiryTime);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || o.getClass() != this.getClass()) return false;
-        IdempotencyEntry that = (IdempotencyEntry) o;
-        return this.response.equals(that.response);
-    }
-    @Override
-    public int hashCode() {
-        return this.response.hashCode();
-    }
-
-    public Object getResponse() { return response; }
-    public long getExpiryTime() { return expiryTime; }
+//    private Object response;
+//    private long expiryTime;
+//
+//    private IdempotencyEntry(Object response, long expiryTime) {
+//        if (response == null) {
+//            throw new IllegalArgumentException("Idempotency Entry cannot be created with null values");
+//        }
+//
+//        this.response = response;
+//        this.expiryTime = expiryTime;
+//    }
+//
+//    public static IdempotencyEntry of(Object respose, long expiryTime) {
+//        return new IdempotencyEntry(respose, expiryTime);
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || o.getClass() != this.getClass()) return false;
+//        IdempotencyEntry that = (IdempotencyEntry) o;
+//        return this.response.equals(that.response);
+//    }
+//    @Override
+//    public int hashCode() {
+//        return this.response.hashCode();
+//    }
+//
+//    public Object getResponse() { return response; }
+//    public long getExpiryTime() { return expiryTime; }
 }
