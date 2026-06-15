26ae2f48b74a5c8d43814f60b7ad3bb9b4178ca8
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-15 23:53:44 +0300

    feat: add cross-service communication methods to TransactionService

--- src/main/java/lv/v3nom/application/service/TransactionService.java
+++ src/main/java/lv/v3nom/application/service/TransactionService.java
@@ -1,11 +1,17 @@
 package lv.v3nom.application.service;
 
-import lv.v3nom.application.dto.requests.TransactionDetailsRequest;
-import lv.v3nom.application.dto.requests.TransactionHistoryRequest;
+import lv.v3nom.application.dto.requests.*;
 import lv.v3nom.application.dto.responses.TransactionHistoryResponse;
 import lv.v3nom.application.dto.responses.TransactionResponse;
 
 public interface TransactionService {
-    public TransactionHistoryResponse getTransactionHistory(TransactionHistoryRequest request);
-    public TransactionResponse getTransactionDetails(TransactionDetailsRequest request);
+    TransactionHistoryResponse getTransactionHistory(TransactionHistoryRequest request);
+    TransactionResponse getTransactionDetails(TransactionDetailsRequest request);
+    TransactionResponse getFailureResponse(GetFailureTransactionRequest request);
+    TransactionResponse createDepositTransaction(CreateTransactionRequest request);
+    TransactionResponse createWithdrawTransaction(CreateTransactionRequest request);
+    TransactionResponse createTransferTransaction(CreateTransactionRequest request);
+    TransactionResponse completeTransaction(CompleteTransactionRequest request);
+    TransactionResponse returnTransaction(CompleteTransactionRequest request);
+    TransactionResponse rejectTransaction(CompleteTransactionRequest request);
 }
