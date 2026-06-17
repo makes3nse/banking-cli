75ae6a099ecb98bce0c18b3635e06c01126cab9e
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-17 23:41:37 +0300

    add validateToken(), authenticate(), getCachedResponse(), saveCachedResponse() method implementations for cross-service calls
    validateToken() validates tokens and checks its expiry, returns a boolean.
    authenticate() accepts sessionToken and returns a CustomerId string if session exists.
    getCachedResponse() accepts idempotency request and returns a cached response or null.
    saveCachedResponse() accepts idempotency request and saves a serialized JSON object response that can be deserialized using GSON and specifying destination object class.

--- src/main/java/lv/v3nom/application/service/impl/AuthServiceImpl.java
+++ src/main/java/lv/v3nom/application/service/impl/AuthServiceImpl.java
@@ -1,4 +1,74 @@
 package lv.v3nom.application.service.impl;
 
-public class AuthServiceImpl {
+import com.google.gson.Gson;
+import lv.v3nom.application.dto.requests.*;
+import lv.v3nom.application.dto.responses.AuthResponse;
+import lv.v3nom.application.dto.responses.BooleanResponse;
+import lv.v3nom.application.dto.responses.CachedResponse;
+import lv.v3nom.application.dto.responses.LogInResponse;
+import lv.v3nom.application.service.AuthService;
+import lv.v3nom.domain.value.CustomerId;
+import lv.v3nom.domain.value.IdempotencyKey;
+import lv.v3nom.infrastructure.idempotency.IdempotencyStore;
+import lv.v3nom.infrastructure.security.TokenStore;
+import lv.v3nom.infrastructure.time.impl.SystemDateTimeProvider;
+
+public class AuthServiceImpl implements AuthService {
+    private final TokenStore tokenStore;
+    private final IdempotencyStore idempotencyStore;
+    private final Gson gson;
+
+    public AuthServiceImpl(TokenStore tokenStore,
+                           IdempotencyStore idempotencyStore,
+                           Gson gson) {
+
+        this.tokenStore = tokenStore;
+        this.idempotencyStore = idempotencyStore;
+        this.gson = gson;
+    }
+
+    @Override
+    public LogInResponse login(AuthRequest request) {
+        return null;
+    }
+    @Override
+    public void logout(LogOutRequest request) {
+
+    }
+    @Override
+    public BooleanResponse validateToken(ValidateTokenRequest request) {
+        SystemDateTimeProvider time = new SystemDateTimeProvider();
+        return new BooleanResponse(tokenStore.isValid(request.getTokenValue(), time.now()));
+    }
+    @Override
+    public AuthResponse authenticate(AuthRequest request) {
+        // returns CustomerId if token is ok
+        CustomerId customerId = tokenStore.getCustomerId(request.getTokenValue());
+        if (customerId != null) {
+            return new AuthResponse(customerId.getValue());
+        }
+        return null;
+    }
+    @Override
+    public CachedResponse getCachedResponse(GetCachedResponseRequest request) {
+        Object cachedResponse = idempotencyStore.retrieve(
+                CustomerId.of(request.getCustomerId()),
+                IdempotencyKey.of(request.getIdempotencyKey())
+        );
+        if (cachedResponse == null) {
+            return null;
+        }
+        return new CachedResponse(
+                gson.toJson(cachedResponse),
+                cachedResponse.getClass().getSimpleName()
+        );
+    }
+    @Override
+    public void saveCachedResponse(SaveCachedResponseRequest request) {
+        idempotencyStore.storeRaw(
+                CustomerId.of(request.getCustomerId()),
+                IdempotencyKey.of(request.getIdempotencyKey()),
+                request.getResponseJson()
+        );
+    }
 }
