633b57df9e049704d7b300bd3a7cdef53a0b2a06
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-17 21:35:34 +0300

    style: clear unnecessary access modifiers in CustomerService interface; add canManipulateTransactions() method for cross-service authorization logic

--- src/main/java/lv/v3nom/application/service/CustomerService.java
+++ src/main/java/lv/v3nom/application/service/CustomerService.java
@@ -4,10 +4,11 @@ import lv.v3nom.application.dto.requests.*;
 import lv.v3nom.application.dto.responses.*;
 
 public interface CustomerService {
-    public CustomerResponse register(RegisterCustomerRequest request);
-    public ChangeNameResponse changeName(ChangeNameRequest request);
-    public ChangeEmailResponse changeEmail(ChangeEmailRequest request);
-    public ChangePasswordResponse changePassword(ChangePasswordRequest request);
-    public ChangePhoneNumberResponse changePhone(ChangePhoneNumberRequest request);
-    public CustomerResponse getCustomer(GetCustomerRequest request); // fetch id from token --> fetch repo
+    CustomerResponse register(RegisterCustomerRequest request);
+    ChangeNameResponse changeName(ChangeNameRequest request);
+    ChangeEmailResponse changeEmail(ChangeEmailRequest request);
+    ChangePasswordResponse changePassword(ChangePasswordRequest request);
+    ChangePhoneNumberResponse changePhone(ChangePhoneNumberRequest request);
+    CustomerResponse getCustomer(GetCustomerRequest request); // fetch id from token --> fetch repo
+    BooleanResponse canManipulateTransactions(CanCustomerManipulateTransactionsRequest request);
 }
