5afa7ebf0aa26999077a2691f8b3ede8599db87d
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-20 23:03:32 +0300

    feat: add getCustomerByEmail() method to retrieve CustomerResponse using provided email

--- src/main/java/lv/v3nom/application/service/CustomerService.java
+++ src/main/java/lv/v3nom/application/service/CustomerService.java
@@ -10,5 +10,6 @@ public interface CustomerService {
     ChangePasswordResponse changePassword(ChangePasswordRequest request);
     ChangePhoneNumberResponse changePhone(ChangePhoneNumberRequest request);
     CustomerResponse getCustomer(GetCustomerRequest request); // fetch id from token --> fetch repo
+    CustomerResponse getCustomerByEmail(GetCustomerByEmailRequest request);
     BooleanResponse canManipulateTransactions(CanCustomerManipulateTransactionsRequest request);
 }
