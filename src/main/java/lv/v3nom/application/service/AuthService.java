3384443ac954e8b26a8d7ee9c4f844f013ab8755
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-21 19:02:36 +0300

    refactor: change logout() method to return BooleanResponse in AuthService interface

--- src/main/java/lv/v3nom/application/service/AuthService.java
+++ src/main/java/lv/v3nom/application/service/AuthService.java
@@ -5,12 +5,10 @@ import lv.v3nom.application.dto.responses.AuthResponse;
 import lv.v3nom.application.dto.responses.BooleanResponse;
 import lv.v3nom.application.dto.responses.CachedResponse;
 import lv.v3nom.application.dto.responses.LogInResponse;
-import lv.v3nom.domain.value.CustomerId;
-import lv.v3nom.domain.value.IdempotencyKey;
 
 public interface AuthService {
     LogInResponse login(LogInRequest request);
-    void logout(LogOutRequest request);
+    BooleanResponse logout(LogOutRequest request);
     BooleanResponse validateToken(ValidateTokenRequest tokenValue);
     AuthResponse authenticate(AuthRequest tokenValue);
     CachedResponse getCachedResponse(GetCachedResponseRequest request);
