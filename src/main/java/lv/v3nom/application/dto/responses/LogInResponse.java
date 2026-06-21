146e3e306cfc98064cf9571ce3106f4a427f3dc7
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-21 19:29:08 +0300

    style: change bool 'success' variable name to more logical one - 'isSucceeded'

--- src/main/java/lv/v3nom/application/dto/responses/LogInResponse.java
+++ src/main/java/lv/v3nom/application/dto/responses/LogInResponse.java
@@ -3,27 +3,27 @@ package lv.v3nom.application.dto.responses;
 public class LogInResponse {
     private String customerId;
     private String sessionToken;
-    private boolean success;
     private String status;
     private String name;
     private String operationStatus;
+    private boolean isSucceeded;
     private String errorMessage;
 
     public LogInResponse() {}
     public LogInResponse(String customerId,
                          String sessionToken,
-                         boolean success,
                          String status,
                          String name,
                          String operationStatus,
+                         boolean isSucceeded,
                          String errorMessage) {
 
         this.customerId = customerId;
         this.sessionToken = sessionToken;
-        this.success = success;
         this.status = status;
         this.name = name;
         this.operationStatus = operationStatus;
+        this.isSucceeded = isSucceeded;
         this.errorMessage = errorMessage;
     }
 
@@ -31,7 +31,7 @@ public class LogInResponse {
     public String getSessionToken() { return sessionToken; }
     public String getStatus() { return status; }
     public String getName() { return name; }
-    public boolean isSuccess() { return success; }
+    public boolean isSucceeded() { return isSucceeded; }
     public String getOperationStatus() { return operationStatus; }
     public String getErrorMessage() { return errorMessage; }
 }
