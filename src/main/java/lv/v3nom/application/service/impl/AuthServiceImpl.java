118786538e953540c4d48b6859afbd4884099423
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-22 04:41:33 +0300

    feat: add login(), logout() method implementations in AuthService

--- src/main/java/lv/v3nom/application/service/impl/AuthServiceImpl.java
+++ src/main/java/lv/v3nom/application/service/impl/AuthServiceImpl.java
@@ -2,38 +2,137 @@ package lv.v3nom.application.service.impl;
 
 import com.google.gson.Gson;
 import lv.v3nom.application.dto.requests.*;
-import lv.v3nom.application.dto.responses.AuthResponse;
-import lv.v3nom.application.dto.responses.BooleanResponse;
-import lv.v3nom.application.dto.responses.CachedResponse;
-import lv.v3nom.application.dto.responses.LogInResponse;
+import lv.v3nom.application.dto.responses.*;
 import lv.v3nom.application.service.AuthService;
-import lv.v3nom.domain.value.CustomerId;
-import lv.v3nom.domain.value.IdempotencyKey;
+import lv.v3nom.application.service.CustomerService;
+import lv.v3nom.domain.security.PasswordHasher;
+import lv.v3nom.domain.value.*;
 import lv.v3nom.infrastructure.idempotency.IdempotencyStore;
+import lv.v3nom.infrastructure.security.TokenProvider;
 import lv.v3nom.infrastructure.security.TokenStore;
 import lv.v3nom.infrastructure.time.impl.SystemDateTimeProvider;
 
 public class AuthServiceImpl implements AuthService {
+    private final CustomerService customerService;
+    private final TokenProvider tokenProvider;
     private final TokenStore tokenStore;
     private final IdempotencyStore idempotencyStore;
     private final Gson gson;
 
-    public AuthServiceImpl(TokenStore tokenStore,
+    public AuthServiceImpl(CustomerService customerService,
+                           TokenProvider tokenProvider,
+                           TokenStore tokenStore,
                            IdempotencyStore idempotencyStore,
                            Gson gson) {
 
+        this.tokenProvider = tokenProvider;
+        this.customerService = customerService;
         this.tokenStore = tokenStore;
         this.idempotencyStore = idempotencyStore;
         this.gson = gson;
     }
 
     @Override
-    public LogInResponse login(AuthRequest request) {
-        return null;
+    public LogInResponse login(LogInRequest request) {
+        // TODO
+        //  1. Check idempotency (prevent duplicate logins)
+        //  2. Find customer by email via CustomerService
+        //  3. Validate credentials (email + password)
+        //  4. Generate new session token
+        //  5. Store token in TokenStore
+        //  6. Return LogInResponse (token + customer details)
+        SystemDateTimeProvider time = new SystemDateTimeProvider();
+
+        IdempotencyKey idempotencyKey = IdempotencyKey.of(request.getIdempotencyKey());
+        EmailAddress emailAddress = EmailAddress.of(request.getEmail());
+
+        CachedResponse cachedResponse = getCachedResponse(
+                new GetCachedResponseRequest(emailAddress.getValue(), idempotencyKey.getValue())
+        );
+        if (cachedResponse != null) {
+            LogInResponse logInResponse = gson.fromJson(
+                    cachedResponse.getResponseJson(),
+                    LogInResponse.class);
+
+            return  logInResponse;
+        }
+
+        GetCustomerByEmailRequest getCustomerByEmailRequest = new GetCustomerByEmailRequest(
+                emailAddress.getValue()
+        );
+        CustomerResponse customerResponse = customerService.getCustomerByEmail(getCustomerByEmailRequest);
+
+        ValidateLoginCredentialsRequest validateLoginCredentialsRequest = new ValidateLoginCredentialsRequest(
+                customerResponse.getCustomerId(),
+                request.getPassword(),
+                emailAddress.getValue()
+        );
+        BooleanResponse validateLoginResponse = customerService.validateLoginCredentials(validateLoginCredentialsRequest);
+        boolean isAuthenticated = validateLoginResponse.value();
+        if (!isAuthenticated) {
+            String failureReason = String.format(
+                    "Credentials does not match for customer: %s", customerResponse.getCustomerId()
+            );
+
+            return new LogInResponse(
+                    customerResponse.getCustomerId(),
+                    null,
+                    null,
+                    null,
+                    OperationStatus.FAILURE.getValue(),
+                    OperationStatus.FAILURE.isOperational(),
+                    failureReason
+            );
+        }
+
+        Token sessionToken = tokenProvider.generateToken(
+                CustomerId.of(customerResponse.getCustomerId()), time.now()
+        );
+        tokenStore.cleanExpired(time.now());
+        tokenStore.store(sessionToken);
+
+        LogInResponse logInResponse = new LogInResponse(
+                customerResponse.getCustomerId(),
+                sessionToken.getValue(),
+                customerResponse.getStatus(),
+                customerResponse.getName(),
+                OperationStatus.SUCCESS.getValue(),
+                OperationStatus.SUCCESS.isOperational(),
+                OperationStatus.SUCCESS.getDescription()
+        );
+
+        return logInResponse;
     }
     @Override
-    public void logout(LogOutRequest request) {
+    public BooleanResponse logout(LogOutRequest request) {
+        // TODO
+        //  1. Check idempotency
+        //  2. Remove token from TokenStore
+        //  3. Notify CustomerService about logout
+        SystemDateTimeProvider time = new SystemDateTimeProvider();
 
+        String sessionToken = request.getCurrentSessionToken();
+        IdempotencyKey idempotencyKey = IdempotencyKey.of(request.getIdempotencyKey());
+        AuthResponse authResponse = authenticate(new AuthRequest(sessionToken));
+        CustomerId customerId = CustomerId.of(authResponse.getCustomerId());
+
+        CachedResponse cachedResponse = getCachedResponse(
+                new GetCachedResponseRequest(customerId.getValue(), idempotencyKey.getValue())
+        );
+        if (cachedResponse != null) {
+            BooleanResponse booleanResponse = gson.fromJson(
+                    cachedResponse.getResponseJson(),
+                    BooleanResponse.class
+            );
+
+            return booleanResponse;
+        }
+        tokenStore.cleanExpired(time.now());
+        tokenStore.invalidate(sessionToken);
+
+        BooleanResponse booleanResponse = new BooleanResponse(OperationStatus.SUCCESS.isOperational());
+
+        return booleanResponse;
     }
     @Override
     public BooleanResponse validateToken(ValidateTokenRequest request) {
@@ -51,16 +150,16 @@ public class AuthServiceImpl implements AuthService {
     }
     @Override
     public CachedResponse getCachedResponse(GetCachedResponseRequest request) {
-        Object cachedResponse = idempotencyStore.retrieve(
+        String cachedResponseJSON = idempotencyStore.retrieve(
                 CustomerId.of(request.getCustomerId()),
                 IdempotencyKey.of(request.getIdempotencyKey())
         );
-        if (cachedResponse == null) {
+        if (cachedResponseJSON == null) {
             return null;
         }
         return new CachedResponse(
-                gson.toJson(cachedResponse),
-                cachedResponse.getClass().getSimpleName()
+                gson.toJson(cachedResponseJSON),
+                cachedResponseJSON.getClass().getSimpleName()
         );
     }
     @Override
