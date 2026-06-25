5e8e19f1209e9fde3d31f8292c6581a94e43e0f2
lxcalbxy <ycxsbeats@yandex.ru>
2026-06-25 21:24:11 +0300

    fix: remove misplaced variables

--- src/main/java/lv/v3nom/application/dto/responses/ChangeEmailResponse.java
+++ src/main/java/lv/v3nom/application/dto/responses/ChangeEmailResponse.java
@@ -1,28 +1,20 @@
 package lv.v3nom.application.dto.responses;
 
 public class ChangeEmailResponse {
-    private String currentSessionToken;
-    private String idempotencyKey;
     private String email;
     private String operationStatus;
     private String errorMessage;
 
     public ChangeEmailResponse() {}
-    public ChangeEmailResponse(String currentSessionToken,
-                               String idempotencyKey,
-                               String email,
+    public ChangeEmailResponse(String email,
                                String operationStatus,
                                String errorMessage) {
 
-        this.currentSessionToken = currentSessionToken;
-        this.idempotencyKey = idempotencyKey;
         this.email = email;
         this.operationStatus = operationStatus;
         this.errorMessage = errorMessage;
     }
 
-    public String getCurrentSessionToken() { return currentSessionToken; }
-    public String getIdempotencyKey() { return idempotencyKey; }
     public String getEmail() { return email; }
     public String getOperationStatus() { return operationStatus; }
     public String getErrorMessage() { return errorMessage; }
