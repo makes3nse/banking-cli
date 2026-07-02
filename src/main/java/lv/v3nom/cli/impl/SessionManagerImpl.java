9e27023d38e5a398fbdc8ef04e9396bc68b02111
lxcalbxy <ycxsbeats@yandex.ru>
2026-07-02 21:05:21 +0300

    feat: add saveToken method implementation in Session Manager

--- src/main/java/lv/v3nom/cli/impl/SessionManagerImpl.java
+++ src/main/java/lv/v3nom/cli/impl/SessionManagerImpl.java
@@ -1,5 +1,20 @@
 package lv.v3nom.cli.impl;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SessionManagerImpl {
-    // TODO
+    private final Path sessionFile = Paths.get(System.getProperty("user.dir"), ".session");
+    private String currentToken;
+
+    public void saveToken(String token) {
+        try {
+            Files.writeString(sessionFile, token);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.currentToken = token;
+    }
 }
