1010d590ae16acbb1b286c7c4d481b7ef06449e6
lxcalbxy <ycxsbeats@yandex.ru>
2026-07-18 21:58:39 +0300

    fix: remove redundant customerId filed
    The CustomerId is being fetched from token. That field was left there by mistake.

--- src/main/java/lv/v3nom/application/dto/requests/GetAccountsRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/GetAccountsRequest.java
@@ -2,16 +2,12 @@ package lv.v3nom.application.dto.requests;
 
 public class GetAccountsRequest {
     private String currentSessionToken;
-    private String customerId;
 
     public GetAccountsRequest() {}
-    public GetAccountsRequest(String currentSessionToken,
-                              String customerId) {
+    public GetAccountsRequest(String currentSessionToken) {
 
         this.currentSessionToken = currentSessionToken;
-        this.customerId = customerId;
     }
 
     public String getCurrentSessionToken() { return currentSessionToken;}
-    public String getCustomerId() { return customerId; }
 }
