db74d808ca52f189737659d44110c9f827a10e2d
lxcalbxy <ycxsbeats@yandex.ru>
2026-07-19 18:22:25 +0300

    fix: add failureRegisterResponse() factory

--- src/main/java/lv/v3nom/application/mapper/CustomerMapper.java
+++ src/main/java/lv/v3nom/application/mapper/CustomerMapper.java
@@ -3,9 +3,11 @@ package lv.v3nom.application.mapper;
 import lv.v3nom.application.dto.requests.RegisterCustomerRequest;
 import lv.v3nom.application.dto.responses.AccountResponse;
 import lv.v3nom.application.dto.responses.CustomerResponse;
+import lv.v3nom.application.dto.responses.RegisterCustomerResponse;
 import lv.v3nom.domain.model.Customer;
 import lv.v3nom.domain.security.PasswordHasher;
 import lv.v3nom.domain.value.OperationStatus;
+import lv.v3nom.domain.value.Token;
 
 import java.time.LocalDateTime;
 
@@ -31,10 +33,37 @@ public class CustomerMapper {
                 operationStatus.getDescription()
         );
     }
+    public static RegisterCustomerResponse toRegisterResponse(Customer customer,
+                                                              String sessionToken,
+                                                              OperationStatus operationStatus) {
+
+        return new RegisterCustomerResponse(
+                customer.getId().getValue(),
+                sessionToken,
+                customer.getName(),
+                customer.getEmail().getValue(),
+                customer.getPhoneNumber().getValue(),
+                customer.getCustomerStatus().getValue(),
+                operationStatus.getValue(),
+                operationStatus.getDescription()
+        );
+    }
     // helpers
     public static CustomerResponse failureResponse(String customerId, OperationStatus operationStatus) {
         return new CustomerResponse(
                 customerId,
+                null,
+                null,
+                null,
+                null,
+                operationStatus.getValue(),
+                operationStatus.getDescription()
+        );
+    }
+    public static RegisterCustomerResponse failureRegisterResponse(String customerId, OperationStatus operationStatus) {
+        return new RegisterCustomerResponse(
+                customerId,
+                null,
                 null,
                 null,
                 null,
