9fb17d4f624d29952f37f5e017d115d1a2404bc5
lxcalbxy <ycxsbeats@yandex.ru>
2026-07-26 22:34:39 +0300

    fix: mend validation, null check should be performed before isBlank() check to avoid NPE

--- src/main/java/lv/v3nom/domain/value/Password.java
+++ src/main/java/lv/v3nom/domain/value/Password.java
@@ -7,18 +7,26 @@ public  final class Password {
     private final String value; // hashed val
 
     private Password(String value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Reconstitution of provided Hashed Password: cannot be null");
+        }
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("Reconstitution of provided Hashed Password: cannot be blank");
+        }
         this.value = value;
     }
 
     public static Password fromRaw(String raw, PasswordHasher hasher) {
-        if (raw.isBlank() || raw == null) throw new IllegalArgumentException("Password cannot be blank or null");
+        if (raw == null) {
+            throw new IllegalArgumentException("Password cannot be null");
+        }
+        if (raw.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be blank");
+        }
         String hashed = hasher.hash(raw);
         return new Password(hashed);
     }
     public static Password of(String hashedPassword) {
-        if (hashedPassword.isBlank() || hashedPassword == null) {
-            throw new IllegalArgumentException("Reconstitution of provided hashed password gone wrong");
-        }
         return new Password(hashedPassword);
     }
     public boolean matches(String raw, PasswordHasher hasher) {
